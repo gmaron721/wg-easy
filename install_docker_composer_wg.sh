45e2ab9978fa551352469b75080015487e9886a8
gmaron721 <36194749+gmaron721@users.noreply.github.com>
2022-01-02 13:46:39 +0400

    Add files via upload

--- install_docker_composer_wg.sh
+++ install_docker_composer_wg.sh
@@ -12,6 +12,8 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
+sudo apt  install docker.io 
+
 #cd ~admin
 #mkdir wg-easy
 #cd wg-easy
