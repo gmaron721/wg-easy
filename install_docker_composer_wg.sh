4eb5ad1358cee44c7ea64efe87274aacd2709e4e
gmaron721 <36194749+gmaron721@users.noreply.github.com>
2022-01-02 12:19:22 +0400

    Add files via upload

--- install_docker_composer_wg.sh
+++ install_docker_composer_wg.sh
@@ -9,7 +9,3 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-mkdir wg-easy
-cd wg-easy
-wget https://raw.githubusercontent.com/gmaron721/wg-easy/master/docker-compose.yml
