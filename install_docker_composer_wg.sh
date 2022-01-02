78e361236d07b2616bcac17d267b08bb14cf9791
gmaron721 <36194749+gmaron721@users.noreply.github.com>
2022-01-02 11:09:59 +0400

    Add files via upload

---
+++ install_docker_composer_wg.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt install python3
+sudo apt-get install apt-transport-https && sudo apt-get install ca-certificates && sudo apt-get install curl && sudo apt-get install gnupg && sudo apt-get install lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \ "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+docker-compose --version
+
+mkdir ~/.wg-easy
+cd ~/.wg-easy
+wget https://raw.githubusercontent.com/gmaron721/wg-easy/master/docker-compose.yml
